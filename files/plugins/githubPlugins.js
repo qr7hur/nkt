9b291b2a1673a38ffd43dddb9d40f4876fa52e1a
a <a@a.a>
2020-06-07 18:46:14 +0200

    githubplugins update

--- files/plugins/githubPlugins.js
+++ files/plugins/githubPlugins.js
@@ -26,15 +26,23 @@ $.plugin({
                 try {
                     try {
                         // JSON ?
-                        let json = JSON.parse(plugin.text);
-                        plugin = atob(json.content);
-                    } catch(e) { // Raw
+                        if (plugin.text.charAt(0) === '{') {
+                            console.log('plugin ' + plugin.name + ' is json');
+                            let json = JSON.parse(plugin.text);
+                            console.log('JSON of plugin ' + plugin.name + ' parsed ok');
+                            plugin.text = atob(json.content);
+                        }
+                    } catch(e) {
+                        console.log('error in json parsing or atobing plugin ' + plugin.name);
+                        console.error(e);// Raw
                     } finally {
                         eval(plugin.text);
                     }
                 } catch (e) { console.error(e); }
                 
             }
+                
+            $.pluginApi.loadPlugin('seed');
             break;
           default:
             console.log('Cannot understand message from iframe');
